892e8a2435d45d56dbd71a7258dbfbcdd88bb5c6
Ricardo Mingote <rickmarina@gmail.com>
2022-05-18 11:13:21 +0200

    solution 32B

---
+++ challengesB/32B.cs
@@ -0,0 +1,31 @@
+using System.Linq;
+
+namespace codeforces.challengesB {
+
+    public class problem32B {
+
+        public static void solution() {
+            string borze = Console.ReadLine();
+            string number = ""; 
+
+            for (int i = 0; i < borze.Length; i++)
+            {
+                if (borze[i] == '.') 
+                    number += "0";
+                else if (borze[i+1] == '.') {
+                    number += "1";
+                    i++;
+                    }
+                else {
+                    number += "2";
+                    i++;
+                    }
+            }
+
+            System.Console.WriteLine(number);
+            
+        }
+
+    }
+
+}
