9b595c03512fc8108c8a47902b8648d1fa9eed7c
Rick Marina <rickmarina@gmail.com>
2024-06-13 15:08:19 +0200

    A. Jellyfish and Undertale solution

--- Program.cs
+++ Program.cs
@@ -8,7 +8,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        
+        // tools 1 1 7 
+        // start 3 
+        // limit 5
+        // 3 + min(1,5) + min(1,5) + min(7,5) - 1 
+
+        int tc = int.Parse(Console.ReadLine());
+
+        while (tc-- > 0)
+        {
+            var input = Console.ReadLine().Split(' ').Select(long.Parse).ToArray();
+            (long limit, long start, long ntools) = (input[0], input[1], input[2]);
+
+            long result = start + Console.ReadLine().Split(' ').Select(long.Parse).Sum(x => x>= limit ? Math.Min(x, limit)-1 : Math.Min(x, limit));
+
+            Console.WriteLine(result);
+        }
 
     }
 
