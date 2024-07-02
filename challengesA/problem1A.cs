755f8c136e4a45a0150591d13298fd22fcb4c9cd
Rick Marina <rickmarina@gmail.com>
2024-07-02 15:08:26 +0200

    A. Theatre Square

--- challengesA/problem1A.cs
+++ challengesA/problem1A.cs
@@ -6,13 +6,11 @@ namespace codeforces.challengesA;
 //A. Theatre Square
 public class Problem1A
 {
-
     public static void solution()
     {
         var input = Console.ReadLine().Split(' ').Select(decimal.Parse).ToArray();
 
         (decimal n, decimal m, decimal a) = (input[0], input[1], input[2]);
-
 
         decimal horizontal = Math.Ceiling(n / a);
         decimal vertical = Math.Ceiling(m / a);
